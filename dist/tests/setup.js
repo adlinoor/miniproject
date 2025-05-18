19bbb3f918801aa38c6793b12517dff0cb8846b8
adlinoor <adli28noor@gmail.com>
2025-05-19 00:10:07 +0700

    clean and so ready to present

--- dist/tests/setup.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prismaMock = void 0;
-// src/tests/setup.ts
-const jest_mock_extended_1 = require("jest-mock-extended");
-exports.prismaMock = (0, jest_mock_extended_1.mockDeep)();
-jest.mock("../lib/prisma", () => ({
-    prisma: exports.prismaMock,
-}));
--- src/tests/setup.ts
+++
@@ -1,9 +0,0 @@
-// src/tests/setup.ts
-import { mockDeep } from "jest-mock-extended";
-import prisma from "../lib/prisma";
-
-export const prismaMock = mockDeep<typeof prisma>();
-
-jest.mock("../lib/prisma", () => ({
-  prisma: prismaMock,
-}));
